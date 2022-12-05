a24ad4ed3cb85cef6bf0b7d4c53e367e933e7489
MomenAshraf50 <momengad50@gmail.com>
2022-12-05 17:00:15 +0200

    settings and profile screens done

--- lib/features/bank/main_screen/profile_screen/profile_screen.dart
+++ lib/features/bank/main_screen/profile_screen/profile_screen.dart
@@ -1,15 +1,155 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:gym/core/utils/constants.dart';
+import 'package:gym/core/widgets/myElevatedButton.dart';
+import 'package:gym/core/widgets/myText.dart';
+import 'package:gym/core/widgets/my_icon_button.dart';
+import 'package:gym/gen/assets.gen.dart';
 
-class ProfileScreen extends StatefulWidget {
+class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
-  State<ProfileScreen> createState() => _ProfileScreenState();
-}
-
-class _ProfileScreenState extends State<ProfileScreen> {
-  @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          children: [
+            Padding(
+              padding: EdgeInsetsDirectional.only(top: 30.h,start: 20.w,end: 20.w),
+              child: Row(
+                children: [
+                  SizedBox(
+                    height: 110.h,
+                    child: Stack(
+                      alignment: Alignment.bottomCenter,
+                      children: [
+                        Align(
+                          alignment: Alignment.topCenter,
+                          child: CircleAvatar(
+                            radius: 55.sp,
+                            backgroundImage: const NetworkImage('https://media.istockphoto.com/id/1309328823/photo/headshot-portrait-of-smiling-male-employee-in-office.jpg?b=1&s=170667a&w=0&k=20&c=MRMqc79PuLmQfxJ99fTfGqHL07EDHqHLWg0Tb4rPXQc='),
+                          ),
+                        ),
+                        IconButton(onPressed: (){}, icon: SvgPicture.asset(Assets.images.svg.camera,height: 30.h,width: 30.w,),)
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                    child: Padding(
+                      padding:  EdgeInsets.symmetric(horizontal: 20.0.sp),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const myText(title: 'Abdo Saied',style: Style.medium,),
+                          const myText(title: 'Coach',style: Style.small,),
+                          space10Vertical,
+                          Align(
+                            alignment: AlignmentDirectional.bottomEnd,
+                              child: myElevatedButton(text: 'Edit Profile',width: 110.w, onPressed: (){}))
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: const[
+                Icon(Icons.location_on_outlined),
+                myText(title: '10th of Ramadan', style: Style.small),
+              ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20.0.sp),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          '4/5',
+                          style: Theme.of(context).textTheme.displaySmall,
+                        ),
+                        Text(
+                          'Ratings',
+                          style: Theme.of(context).textTheme.displaySmall!.copyWith(fontWeight: FontWeight.normal),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 40.h,
+                    width: 1.w,
+                    color: Colors.grey,
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          '78',
+                          style: Theme.of(context).textTheme.displaySmall,
+                        ),
+                        Text(
+                          'Following',
+                          style: Theme.of(context).textTheme.displaySmall!.copyWith(fontWeight: FontWeight.normal),
+                        )
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 40.h,
+                    width: 1.w,
+                    color: Colors.grey,
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(
+                          '5634',
+                          style: Theme.of(context).textTheme.displaySmall,
+                        ),
+                        Text(
+                          'Followers',
+                          style: Theme.of(context).textTheme.displaySmall!.copyWith(fontWeight: FontWeight.normal),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 50.h),
+              child: Row(
+                children: [
+                  Expanded(child: DefaultIconButton(icon: SvgPicture.asset(Assets.images.svg.facebook_icon,), onPressed: (){})),
+                  Expanded(child: DefaultIconButton(icon: SvgPicture.asset(Assets.images.svg.tiktok), onPressed: (){})),
+                  Expanded(child: DefaultIconButton(icon: SvgPicture.asset(Assets.images.svg.instagram), onPressed: (){})),
+                  Expanded(child: DefaultIconButton(icon: SvgPicture.asset(Assets.images.svg.youtube), onPressed: (){})),
+                ],
+              ),
+            ),
+            Card(
+              elevation: 5,
+              child: Padding(
+                padding: EdgeInsets.all(20.0.sp),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children:  [
+                    const myText(title: 'Certifications', style: Style.medium),
+                    Text('Add Your Certifications Now To Earn Budge',
+                    style: Theme.of(context).textTheme.displaySmall!.copyWith(fontWeight: FontWeight.normal),),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/features/bank/main_screen/setting_screen/setting_screen.dart
+++ lib/features/bank/main_screen/setting_screen/setting_screen.dart
@@ -1,15 +1,70 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:gym/core/utils/colors_manager.dart';
+import 'package:gym/core/utils/constants.dart';
+import 'package:gym/gen/assets.gen.dart';
 
-class SettingScreen extends StatefulWidget {
-  const SettingScreen({Key? key}) : super(key: key);
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({Key? key}) : super(key: key);
 
-  @override
-  State<SettingScreen> createState() => _SettingScreenState();
-}
-
-class _SettingScreenState extends State<SettingScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Center(
+            child: Text('Settings',
+                style: Theme.of(context)
+                    .textTheme
+                    .displayMedium!
+                    .copyWith(fontWeight: FontWeight.bold))),
+      ),
+      body: Column(
+        children: [
+          buildSettingsItem(
+              'Notifications', Assets.images.svg.notifications_icon, context),
+          buildSettingsItem(
+              'Location', Assets.images.svg.location_icon, context),
+          buildSettingsItem('Support', Assets.images.svg.support_icon, context),
+          buildSettingsItem('Share', Assets.images.svg.share_icon, context),
+          buildSettingsItem('Log Out', Assets.images.svg.logout_icon, context),
+        ],
+      ),
+    );
   }
+
+  Widget buildSettingsItem(
+          String text, String iconPath, BuildContext context) =>
+      InkWell(
+        onTap: () {
+          if (text == 'Notifications') {
+          } else if (text == 'Location') {
+          } else if (text == 'Support') {
+          } else if (text == 'Share') {
+          } else {}
+        },
+        child: Padding(
+          padding: EdgeInsets.all(25.0.sp),
+          child: Row(
+            children: [
+              SvgPicture.asset(iconPath),
+              space20Horizontal,
+              text == 'Log Out'
+                  ? Text(text,
+                      style: Theme.of(context)
+                          .textTheme
+                          .displayMedium!
+                          .copyWith(color: Colors.red))
+                  : Text(text,
+                      style: Theme.of(context).textTheme.displayMedium!),
+              const Spacer(),
+              const Icon(
+                Icons.arrow_forward_ios,
+                color: ColorManager.mainColor,
+              )
+            ],
+          ),
+        ),
+      );
 }
